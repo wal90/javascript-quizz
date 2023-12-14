b31713a7aed603d1769412f68ac66e8efad062b7
wal90 <walteromartinez90@gmail.com>
2023-12-14 20:49:09 -0300

    material ui in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { Container, Typography, Stack } from '@mui/material'
 import './App.css'
+import { JavaScriptLogo } from './JavaScriptLogo'
 
 function App() {
   return (
-    <>
-    <h1>JavaScript Quizz</h1>
-    </>
+    <main>
+      <Container maxWidth='sm'>
+        <Stack direction='row' gap={2} alignItems='center' justifyContent='center'>
+          <JavaScriptLogo />
+            <Typography variant='h2' component='h1'>
+              JavaScript Quizz 
+            </Typography>
+        </Stack>
+      
+      </Container>
+    
+    </main>
   )
 }
 
