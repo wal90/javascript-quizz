a5fc036837d40f3827b5c655270fcb8df1b57845
wal90 <walteromartinez90@gmail.com>
2023-12-15 20:53:32 -0300

    interface question finalizado

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,7 +1,9 @@
 export interface Question {
-    id: number;
-    question: string;
-    code: string;
-    answers: string [];
-    correctAnswer: number;
+    id: number
+    question: string
+    code: string
+    answers: string []
+    correctAnswer: number
+    userSelectedAnswer?: number
+    isCorrectUserAnswer?: boolean
 }
