58fcd61673811a5567b26afb66bed5814031415b
wal90 <walteromartinez90@gmail.com>
2023-12-16 19:13:31 -0300

    questions in fetch

--- src/store/questions.ts
+++ src/store/questions.ts
@@ -13,7 +13,22 @@ export const useQuestionsStore = create<State>((set, get) => {
         currentQuestion: 0,
 
         fetchQuestions: async (limit: number) => {
-            set()
+            set({
+                questions: [
+                    {
+                        "id": 46,
+                        "question": "¿Cuál es el resultado de la siguiente expresión?",
+                        "code": "'2' + true",
+                        "answers": [
+                          "'2true'",
+                          "'3'",
+                          "3",
+                          "Error"
+                        ],
+                        "correctAnswer": 0
+                    }
+                ]
+            })
         }
 
     }
