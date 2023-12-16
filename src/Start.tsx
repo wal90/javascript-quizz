e3f2763d970f46dd79bdcf6b1f1d416d8f119c73
wal90 <walteromartinez90@gmail.com>
2023-12-16 19:18:09 -0300

    handle click

--- src/Start.tsx
+++ src/Start.tsx
@@ -2,8 +2,12 @@ import { Button } from "@mui/material"
 
 export const Start = () => {
     const fetchQuestions = useQuestionsStore(state => state.fetchQuestions)
+
+    const handleClick = ( {
+        fetchQuestions()
+    })
     return (
-        <Button onClick={() => {}} variant="contained">
+        <Button onClick={handleClick} variant="contained">
             ¡Empezar!
         </Button>
     )
