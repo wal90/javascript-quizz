576002227324f1bb507ea78b4ecc99454db8d0e7
wal90 <walteromartinez90@gmail.com>
2023-12-16 19:15:30 -0300

    fetch questions in app

--- src/Start.tsx
+++ src/Start.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@mui/material"
 
 export const Start = () => {
+    const fetchQuestions = useQuestionsStore(state => state.fetchQuestions)
     return (
         <Button onClick={() => {}} variant="contained">
             ¡Empezar!
