f42e98ebac000fbdf5e5a6a0d40b05028569b940
wal90 <walteromartinez90@gmail.com>
2023-12-18 21:30:38 -0300

    qurrent cuestion / qyestions

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,9 +1,13 @@
 import { IconButton, Stack } from "@mui/material";
+import { useQuestionsStore } from "./store/questions";
 
 const Question = ({ info }) => {
     
 }
 export const Game = () => {
+    const questions = useQuestionsStore(state => state.questions)
+    const currentQuestion = useQuestionsStore(state => state.currentQuestion)
+    
     return (
         <>
         <Question />
