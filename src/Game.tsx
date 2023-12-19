0a44ce2fb9f9df76c16070f3663d04535b19781a
wal90 <walteromartinez90@gmail.com>
2023-12-19 20:32:16 -0300

    card in question

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,9 +1,14 @@
 import { IconButton, Stack } from "@mui/material";
+import { Card } from "@mui/material"
 import { useQuestionsStore } from "./store/questions";
 import { type Question as QuestionType} from "./types";
 
 const Question = ({ info }: { info: QuestionType }) => {
-    return null
+    return (
+        <Card variant='outlined'>
+
+        </Card>
+    )
 }
 export const Game = () => {
     const questions = useQuestionsStore(state => state.questions)
