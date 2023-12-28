b8e6788ca8b2d8616d4f80f95bb5ac8c9dffea7e
wal90 <walteromartinez90@gmail.com>
2023-12-28 20:20:16 -0300

    questions in footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,4 +1,11 @@
+import { useQuestionsStore } from "./store/questions"
+
 export const Footer = () => {
+    const questions = useQuestionsStore(state=> state.questions)
+
+    let correct = 0
+    let incorrect = 0
+    let unanswered = 0
     return (
         <footer style={{ marginTop: '16px '}}>
             <strong>{`✅ {}`}</strong>
