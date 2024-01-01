1c6b711b61132143fae06834375e03b44c68283f
wal90 <walteromartinez90@gmail.com>
2024-01-01 20:16:40 -0300

    use question store in footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@mui/material"
 import { useQuestionData } from "./hooks/useQuestionsData"
+import { useQuestionsStore } from "./store/questions"
 
 export const Footer = () => {
 
     const { correct, incorrect, unanswered } = useQuestionData()
+    const reset = useQuestionsStore(state => state.reset)
    
     return (
         <footer style={{ marginTop: '16px '}}>
